3790b6a3e89cb45960504653e8a27d71f42e8ebe
Ccilpy <cecilepuyol@gmail.com>
2024-10-15 11:36:24 +0200

    add project

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Aghiles Belkalem"));
+console.log (chalk.green("Pierre De Castelbajac"));
+console.log(chalk.yellow("Raphaël Barre"));
+console.log(chalk.red("Raphaël Streiff"));
